980d192d8a3db44db24fc34fb81db3d0a5e777f9
Stanislav <stanislav.semenov2386@gmail.com>
2023-02-04 22:31:01 +0300

    classNames

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { Link } from "react-router-dom";
-import { Suspense, useContext } from "react";
+import { Suspense } from "react";
+import { useTheme } from "./Theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 import { Route, Routes } from "react-router-dom";
-import { Counter } from "./components/Counter/Counter";
 import { MainPageAsync } from "./Pages/MainPage/MainPageAsync";
 import { AboutPageAsync } from "./Pages/AboutPage/AboutPageAsync";
 import "./styles/index.scss";
-import { Theme, ThemeContext } from "./Theme/ThemeContext";
-import { useTheme } from "./Theme/useTheme";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
