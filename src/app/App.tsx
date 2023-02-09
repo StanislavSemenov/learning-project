029c5abe1c3e3c58ec98e5827390e6a825b0f27e
Stanislav <stanislav.semenov2386@gmail.com>
2023-02-09 21:53:23 +0300

    15 i18n Интернационализация. Define plugin. Плагин для переводов

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,18 +2,23 @@ import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/SideBar";
 import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
+import { Suspense } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import "./styles/index.scss";
 
 const App = () => {
   const { theme } = useTheme();
+
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Navbar to={""} />
-      <div className="content-page">
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback="">
+        <Navbar to={""} />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
+
     </div >
   )
 };
--- src/widgets/SideBar/ui/SideBar/Sidebar.tsx
+++ src/widgets/SideBar/ui/SideBar/Sidebar.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { classNames } from 'shared/lib/classNames/classNames';
-import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+import { LangSwitcher } from "widgets/LangSwitcher";
+import { ThemeSwitcher } from "widgets/ThemeSwitcher";
 import cls from './Sidebar.module.scss';
+
 
 interface SidebarProps {
   className?: string;
@@ -21,6 +23,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
       <button onClick={onToggle}>toggle</button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
+        <LangSwitcher className={cls.lang} />
       </div>
     </div>
   );
