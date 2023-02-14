9d2bea78efa2f71718a3b2d20cd34677cf7d11d0
Stanislav <stanislav.semenov2386@gmail.com>
2023-02-14 21:28:09 +0300

    21 Несуществующие маршруты. Лоадер для загрузки страниц

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,24 +1,25 @@
-import { t } from 'i18next';
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { RouterConfig } from 'shared/config/RouterConfig/RouterConfig';
+import { PageLoader } from 'widgets/PageLoader';
+
 
 export function AppRouter() {
   return (
-    <Suspense fallback={<div>{t('Loading...')}</div>}>
-      <Routes>
-        {Object.values(RouterConfig).map(({ element, path }) => (
-          <Route
-            key={path}
-            path={path}
-            element={(
+    <Routes>
+      {Object.values(RouterConfig).map(({ element, path }) => (
+        <Route
+          key={path}
+          path={path}
+          element={(
+            <Suspense fallback={<PageLoader/>}>
               <div className="page-wrapper">
                 {element}
               </div>
-            )}
+            </Suspense>
+          )}
           />
         ))}
-      </Routes>
-    </Suspense>
+    </Routes>
   );
 }
--- src/shared/config/routerConfig/routerConfig.tsx
+++ src/shared/config/routerConfig/routerConfig.tsx
@@ -1,15 +1,18 @@
 import { MainPage } from 'pages/ui/MainPage';
 import { AboutPage } from 'pages/ui/AboutPage';
 import { RouteProps } from 'react-router-dom';
+import { NotFoundPage } from 'pages/ui/NotFoundPage';
 
 export enum AppRouters {
   MAIN = 'main',
   ABOUT = 'about',
+  NOT_FOUND = 'not_found'
 };
 
 export const RoutePath: Record<AppRouters, string> = {
   [AppRouters.MAIN]: '/',
   [AppRouters.ABOUT]: 'about',
+  [AppRouters.NOT_FOUND]: '*',
 };
 
 export const RouterConfig: Record<AppRouters, RouteProps> = {
@@ -20,5 +23,9 @@ export const RouterConfig: Record<AppRouters, RouteProps> = {
   [AppRouters.ABOUT]: {
     path: RoutePath.about,
     element: <AboutPage />
+  },
+  [AppRouters.NOT_FOUND]: {
+    path: RoutePath.not_found,
+    element: <NotFoundPage />
   }
 };
