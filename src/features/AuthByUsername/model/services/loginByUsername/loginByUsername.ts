10ddbfbae042b223312080abcf2f5940ba0e4abc
Stanislav <stanislav.semenov2386@gmail.com>
2023-03-11 20:28:15 +0300

    add tests authByUsername

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -23,7 +23,6 @@ export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { re
 
             return response.data;
         } catch (e) {
-            // eslint-disable-next-line no-console
             console.log(e);
             return thunkAPI.rejectWithValue('error');
         }
