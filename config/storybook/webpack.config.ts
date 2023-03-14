115698fb3ccc01eeacb6d4d0f0cb3cf6c8aebab7
Stanislav <stanislav.semenov2386@gmail.com>
2023-03-14 13:31:02 +0300

    refactoring storybook/ webpack.config.ts

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -3,43 +3,33 @@ import path from 'path';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 import { BuildPaths } from '../build/types/config';
 
-export default ({ config }: { config: webpack.Configuration }) => {
+export default ({ config }: {config: webpack.Configuration}) => {
     const paths: BuildPaths = {
         build: '',
         html: '',
         entry: '',
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
-    config.resolve?.modules?.push(paths.src);
-    /* config.resolve?.modules?.unshift(paths.src); // TODO!: уточнить актуальность решения */
-    config.resolve?.extensions?.push('.ts', '.tsx');
+    config!.resolve!.modules!.push(paths.src);
+    config!.resolve!.extensions!.push('.ts', '.tsx');
 
     // eslint-disable-next-line no-param-reassign
-    if (config?.module?.rules) {
-        const rules = config.module.rules as RuleSetRule[];
+    // @ts-ignore
+    config!.module!.rules = config.module!.rules!.map((rule: RuleSetRule) => {
+        if (/svg/.test(rule.test as string)) {
+            return { ...rule, exclude: /\.svg$/i };
+        }
 
-        // eslint-disable-next-line no-param-reassign
-        config.module.rules = rules.map((rule: RuleSetRule) => {
-            if (/svg/.test(rule.test as string)) {
-                return { ...rule, exclude: /\.svg$/i };
-            }
-            return rule;
-        });
-    }
-    /* исправление ошибки storybook */
-    if (config?.resolve?.modules) {
-        config.resolve.modules = [
-            path.resolve(__dirname, '../../src'),
-            'node_modules',
-        ];
-    }
+        return rule;
+    });
 
-    config.module?.rules?.push({
+    config!.module!.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     });
-    config.module?.rules?.push(buildCssLoader(true));
-    config.plugins?.push(new DefinePlugin({
+    config!.module!.rules.push(buildCssLoader(true));
+
+    config!.plugins!.push(new DefinePlugin({
         __IS_DEV__: JSON.stringify(true),
         __API__: JSON.stringify(''),
     }));
