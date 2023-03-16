c35902baedbacfa0dab3fac39f8ca479054f6464
Stanislav <stanislav.semenov2386@gmail.com>
2023-03-16 17:51:11 +0300

    temp

--- src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ProfileCard } from 'entities/Profile';
 import { Country } from 'entities/Country';
 import { Currency } from 'entities/Currency';
-import avatar from 'shared/assets/tests/storybook.jpg';
+import avatar from '../../../../../public/assets/storybook.jpg';
 
 export default {
     title: 'entities/ProfileCard',
